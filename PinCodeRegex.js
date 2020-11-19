540d97ecd44a3595ce062f17654861b43e3a1d75
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-19 15:15:11 +0530

    [Kartikeya] Regex for pincode with third condition

--- PinCodeRegex.js
+++ PinCodeRegex.js
@@ -25,3 +25,17 @@
         console.log("Invalid pin");
     }
 }
+
+// UC 3
+{
+    let pinCode = '28100B';
+    let regexPattern = RegExp('^[0-9]{1}[a-zA-Z0-9]{4}[0-9]{1}$');
+    if (regexPattern.test(pinCode))
+    {
+        console.log("Valid pin");
+    } 
+    else
+    {
+        console.log("Invalid pin");
+    }
+}
