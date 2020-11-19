6f9403d71dbe08a0b838badd0a67ee33884f87dd
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-11-19 15:12:10 +0530

    [Kartikeya] Regex for pincode with second condition

--- PinCodeRegex.js
+++ PinCodeRegex.js
@@ -10,4 +10,18 @@
     {
         console.log("Invalid pin");
     }
-}       
+}   
+
+// UC 2
+{
+    let pinCode = 'A28100';
+    let regexPattern = RegExp('^[0-9]{1}[a-zA-Z0-9]{5}$');
+    if (regexPattern.test(pinCode))
+    {
+        console.log("Valid pin");
+    } 
+    else
+    {
+        console.log("Invalid pin");
+    }
+}
